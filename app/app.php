c8db252ca69f652559639272fcc75861e78a008d
Ronando Long <ronando.long@gmail.com>
2017-02-02 11:41:20 -0800

    add delte Twig file and ability for user to delete all tasks, app works properly, just mopping up now

--- app/app.php
+++ app/app.php
@@ -20,40 +20,18 @@
         return $app['twig']->render('places.html.twig', array('places' => Place::getAll()));
     });
 
-  // 2. Route to Confirmation page with newly added Place
+  // 2. POST Route to Confirmation page with newly added Place
     $app->post("/places", function() use ($app) {
         $place = new Place($_POST['place']);     // Instantiation of new Place object
         $save = $place->save();                  // Saving the new place object
 
         return $app['twig']->render('new_place.html.twig', array('newplace' => $place));
-      });
+    });
 
-
-    $app->post('/', function() {
-        // xxxxxxxxxx
-
-        return "
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <meta charset='utf-8'>
-              <title>Tasks: To Do List</title>
-              <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-            </head>
-
-            <body>
-              <div class='container'>
-                <h1>Tasks: To Do List</h1>
-                <div class='row'>
-                  <div class='col-md-4'>
-
-
-                  </div>  <!-- .col-md-4 -->
-                </div>  <!-- .row -->
-              </div>  <!-- .container -->
-            </body>
-          </html>
-        ";
+  // 3. POST Route to confirm Deletion of all places, before going to home page
+    $app->post("/delete", function() use ($app) {
+        Place::deleteAll();
+        return $app['twig']->render('delete_place.html.twig');
     });
 
 
